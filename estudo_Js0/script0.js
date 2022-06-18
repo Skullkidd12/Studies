35d2f9c632d12018836932e5b54f69cd531fa3c8
Skullkidd12 <araujo.skd@gmail.com>
2022-06-17 23:29:16 -0300

    estudos

---
+++ estudo_Js0/script0.js
@@ -0,0 +1,50 @@
+/*var myList = [
+  ["Cereal", 3],
+  ["Leite", 2],
+  ["Açúcar", 5],
+  ["Suco de Pózinho", 34],
+  ["Queijo", 500],
+];*/
+
+/*function dizerOlaBeninos() {
+  console.log("OLA BENINOS");
+}
+
+dizerOlaBeninos();
+dizerOlaBeninos();
+dizerOlaBeninos();
+*/
+
+/*function funcaoComParam(a, b) {
+  console.log(a - b);
+}
+
+funcaoComParam(30, 60);*/
+
+/*function menosSete(num){
+  return num - 7;
+}
+
+console.log(menosSete(4))*/
+
+/*function processo1(num) {
+  return num * 3 + num - 7;
+}
+
+let quatroProcessado = processo1(4);
+
+console.log(quatroProcessado);*/
+
+/*
+let testArr = [1, 2, 3, 4, 5];
+
+function proximoDaFila(array, item) {
+  let numeroRetirado = array.shift();
+  array.unshift(item);
+  return "retirei: " + numeroRetirado + "\n" + "e adicionei: " + item;
+}
+
+console.log("before: " + JSON.stringify(testArr));
+console.log(proximoDaFila(testArr, 80));
+console.log("after: " + JSON.stringify(testArr));
+*/
